284456cabd84737c46c4ced0a6495cc54074f486
SenuraRansika <senuraransika12@gmail.com>
2026-09-17 16:25:42 +0530

    Add ward lookup arrays and bed occupancy matrix

--- main.c
+++ main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
 #define NUM_SPECIALTIES 4
+#define NUM_WARDS 4
+#define MAX_BEDS_PER_WARD 20
+
+int wardID[NUM_WARDS] = {1, 2, 3, 4};
+char wardName[NUM_WARDS][30] = {"General Ward", "Paediatric Ward", "Surgical Ward", "ICU"};
+float wardDailyRate[NUM_WARDS] = {3000.00, 6000.00, 12000.00, 25000.00};
+int wardCapacity[NUM_WARDS] = {20, 10, 10, 5};
+
+int bedOccupancy[NUM_WARDS][MAX_BEDS_PER_WARD] = {0};
 
 int specialtyID[NUM_SPECIALTIES] = {1, 2, 3, 4};
 char specialtyName[NUM_SPECIALTIES][30] = {"General Practice (OPD)", "Paediatrics", "Cardiology", "Neurology"};
